7a177e5cbec2771aac9c8d27e6e821aa88a28660
marcosenp <marcospne2103@gmail.com>
2025-04-04 16:19:19 -0300

    Estrutura inicial do projeto .NET console - Gerenciador de Tarefas.

---
+++ Projeto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Gerenciador de Tarefas");
+
+        List<Tarefa> tarefas = new List<Tarefa>();
+
+        tarefas.Add(new Tarefa { Titulo = "Fazer compras.", Descricao = "maçã, banana, morango e abacaxi" });
+
+        foreach (var tarefa in tarefas)
+        {
+            Console.WriteLine($"Tarefa: {tarefa.Titulo} - {tarefa.Descricao}");
+        }
+    }
+}
+
+public class Tarefa
+{
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+}
